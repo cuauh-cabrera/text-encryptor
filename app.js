b2036aaf30bf51c3ee1aae4efc6e53da808c5960
Cuau Cabrera <cabrera.cuau@gmail.com>
2024-03-21 19:15:21 -0500

    Refactor encrypt and decrypt event handlers

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ const outputHeader = document.querySelector('#output-header');
 const outputText = document.querySelector('#output-text');
 const outputBkg = document.querySelector('#output-bkg');
 const outputActions = document.querySelector('#output-action');
-//////////////* Encrypt Text *///////////////////
 
-// Encryption keys
+// Encryption & Decryption keys
 const encryptKeys = {
     'a': 'ai',
     'e': 'enter',
@@ -17,38 +16,6 @@ const encryptKeys = {
     'o': 'ober',
     'u': 'ufat'
 };
-// Encryption RegEx
-const encryptExp = /[aeiou]/gi;
-
-// Encrypt text
-btnEncrypt.addEventListener('click', () => {
-    let inputText = inputValue.value.toLowerCase();
-    // Replace characters by encryption key
-    let encryptedText = inputText.replace(encryptExp, keys => encryptKeys[keys]);
-    // Hide output warning message
-    outputHeader.classList.toggle('no-show');
-    outputText.classList.toggle('no-show');
-    outputBkg.classList.toggle('no-show');
-    // Add the encrypted text to the DOM
-    const encrypted = document.createElement('p');
-    encrypted.setAttribute('id', 'output-encrypted');
-    encrypted.classList.toggle('output-encrypted');
-    encrypted.textContent = `${encryptedText}`;
-    textContainer.appendChild(encrypted);
-    // Add reset text button
-    const resetBtn = document.createElement('button');
-    resetBtn.setAttribute('id', 'reset-btn');
-    resetBtn.classList.toggle('submit-action__white');
-    resetBtn.textContent = 'Reiniciar';
-    outputActions.appendChild(resetBtn);
-    resetBtn.addEventListener('click', () => {
-        location.reload();
-    })
-});
-
-/////////////* Decrypt Text*/////////////////////
-
-// Decryption keys
 const decryptKeys = {
     'ai': 'a',
     'enter': 'e',
@@ -56,31 +23,59 @@ const decryptKeys = {
     'ober': 'o',
     'ufat': 'u'
 };
-// Decryption RegEx
+// Matching RegEx
+const encryptExp = /[aeiou]/gi;
 const decryptExp = /(ai|enter|imes|ober|ufat)/gi;
-
-// Decrypt text
-btnDecrypt.addEventListener('click', () => {
-    let inputText = inputValue.value;
-    // Replace characters by decryption key
-    let decryptedText = inputText.replace(decryptExp, keys => decryptKeys[keys]);
-    // Hide output warning message
+// Setup vars
+let encryptedText;
+let decryptedText;
+// Encrypt function
+function encryptText () {
+    let inputText = inputValue.value.toLowerCase();
+    encryptedText = inputText.replace(encryptExp, keys => encryptKeys[keys]);
+}
+// Decrypt function
+function decryptText () {
+    let inputText = inputValue.value.toLowerCase();
+    encryptedText = inputText.replace(decryptExp, keys => decryptKeys[keys]);
+}
+// Hide output message function
+function hideOutputMessage () {
     outputHeader.classList.toggle('no-show');
     outputText.classList.toggle('no-show');
     outputBkg.classList.toggle('no-show');
-    // Add the encrypted text to the DOM
-    const decrypted = document.createElement('p');
-    decrypted.setAttribute('id', 'output-encrypted');
-    decrypted.classList.toggle('output-encrypted');
-    decrypted.textContent = `${decryptedText}`;
-    textContainer.appendChild(decrypted);
-    // Add reset text button
+}
+// Append encrypted text function
+function appendEncryptedText () {
+    const encrypted = document.createElement('p');
+    encrypted.setAttribute('id', 'output-encrypted');
+    encrypted.classList.toggle('output-encrypted');
+    encrypted.textContent = `${encryptedText}`;
+    textContainer.appendChild(encrypted);
+}
+// Append reset button
+function appendResetTextBtn () {
     const resetBtn = document.createElement('button');
-    resetBtn.setAttribute('id', 'reset-btn');
-    resetBtn.classList.toggle('submit-action__white');
-    resetBtn.textContent = 'Reiniciar';
-    outputActions.appendChild(resetBtn);
-    resetBtn.addEventListener('click', () => {
-        location.reload();
-    })
+resetBtn.setAttribute('id', 'reset-btn');
+resetBtn.classList.toggle('submit-action__white');
+resetBtn.textContent = 'Reiniciar';
+outputActions.appendChild(resetBtn);
+resetBtn.addEventListener('click', () => {
+    location.reload();
+});
+}
+///////////////*Event handlers*///////////////
+// Encrypt
+btnEncrypt.addEventListener('click', () => {
+    encryptText();
+    hideOutputMessage();
+    appendEncryptedText();
+    appendResetTextBtn();
+}); 
+// Decrypt
+btnDecrypt.addEventListener('click', () => {
+    decryptText();
+    hideOutputMessage();
+    appendEncryptedText();
+    appendResetTextBtn();
 });
